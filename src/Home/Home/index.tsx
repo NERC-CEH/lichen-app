d3cb5f23fd55fe58a7e55d989d352b15c1cd41d4
Karolis Kazlauskis <karolis@flumens.io>
2024-02-20 15:46:30 +0200

    Add home page logo

--- src/Home/Home/index.tsx
+++ src/Home/Home/index.tsx
@@ -1,31 +1,32 @@
 import { Page, Main } from '@flumens';
 import { IonButton, IonIcon } from '@ionic/react';
+import appLogo from './app_logo.png';
 import lichenIcon from './lichen.svg';
 import './styles.scss';
 
 const HomeController = () => {
   return (
     <Page id="home">
-      <Main>
-        <h1 className="mx-auto block w-fit bg-primary-200 p-5">
-          WIP: app logo
-        </h1>
+      <Main className="no-padding">
+        <div className="h-full w-full bg-gradient-to-b from-white via-primary-50 to-primary-100 pt-3">
+          <img src={appLogo} alt="" className="m-auto w-full max-w-[80%]" />
 
-        <div className="buttons">
-          <IonButton
-            className="fancy-button"
-            color="light"
-            routerLink="/survey"
-          >
-            <div className="fancy-button-container">
-              <div>
-                <h4 className="pb-3">Start survey</h4>
-                Assess nitrogen pollution
+          <div className="buttons">
+            <IonButton
+              className="fancy-button"
+              color="light"
+              routerLink="/survey"
+            >
+              <div className="fancy-button-container">
+                <div>
+                  <h4 className="pb-3">Start survey</h4>
+                  Assess nitrogen pollution
+                </div>
+
+                <IonIcon src={lichenIcon} />
               </div>
-
-              <IonIcon src={lichenIcon} />
-            </div>
-          </IonButton>
+            </IonButton>
+          </div>
         </div>
       </Main>
     </Page>
